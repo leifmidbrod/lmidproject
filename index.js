6db171020b89f04abd9de2762b77c60163a35d11
Leif Midbrød <lmid@statoil.com>
2015-10-19 10:37:17 +0200

    added log statement

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var app = express()
 
+
 app.get('/', function(req, res){
+	console.log('Started');
 	res.send('Hello World!')
 });
 
 app.listen(3000);
+console.log('Listening on 3000');
